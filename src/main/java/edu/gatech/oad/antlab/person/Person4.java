6cd0aa7f1672ebb84eb4a7ea2e9af30b2f0c9721
Sophie Handel <sophiehandel@lawn-143-215-86-18.lawn.gatech.edu>
2018-01-25 17:28:35 -0500

    Implemented method for Person4

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -30,8 +30,11 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      String str = "";
+      for (int i = 0; i < input.length(); i++) {
+        str = str + (char) (input.charAt(i) + 1);
+      }
+      return str;
     }
     
     /**
